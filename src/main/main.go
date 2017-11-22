83b21b8f11a6d19153eecce88f5420afe3c7e834
lioneagle <leoeagle@163.com>
2017-11-23 01:10:47 +0800

    try logger

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+
+	"github.com/lioneagle/goutil/src/logger"
 )
 
 func main() {
-	fmt.Println("thor: a web editor")
+	logger.SetLevel(logger.DEBUG)
+	//logger.SetStackTraceLevel(logger.DEBUG)
+	logger.Print("thor: a web editor")
+	logger.Debug("thor: a web editor")
+	logger.PrintStack()
 }
